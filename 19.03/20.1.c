1d177651ff35eff8b48a82e378218e7c13d953a2
Anshiya Kansal <anshiyakansal.india@gmail.com>
2025-03-20 17:31:26 +0530

    Create 20.1.c

---
+++ 19.03/20.1.c
@@ -0,0 +1,42 @@
+// #include <stdio.h>
+// void printArray(int *arr, int size)
+// {
+//     for (int i = 0; i<size; i++)
+//     {
+//         print("%d" , arr[i]);
+//     }
+//     printf("\n");
+// }
+// int main() 
+// {
+//     int numbers[] = {1,2,3,4,5};
+//     printArray(numbers,5);
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//   int size, i;
+//   printf("enter element count:");
+//   scanf("%d", &size);
+//   int arr[size];
+//   printf("enter %d elements:\n", size);
+//   for(i=0; i<size; i++){
+//     scanf("%d", &arr[i]);
+//   }
+//   printf("you entered:");
+//   for (i=0; i<size; i++){
+//     printf("%d", arr[i]);
+//   }
+//   return 0;
+// }
+
+
+
+#include <stdio.h>
+int main(){
+  it arr[5], i;
+  printf("enter 5 nnumbers");
+  for 
