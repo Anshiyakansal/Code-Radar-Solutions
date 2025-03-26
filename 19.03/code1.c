51305f4ea8ba2653ddf4185877bb94691fd48480
Anshiya Kansal <anshiyakansal.india@gmail.com>
2025-03-26 16:49:49 +0530

    Update code1.c

--- 19.03/code1.c
+++ 19.03/code1.c
@@ -32,3 +32,19 @@ int main()
     }
     return 0;
 }
+#include <stdio.h>
+int main()
+{
+    int numbers[]={10,20,30,40,50};
+    int*ptr[5];
+    for (int i=0;i<5;i++)
+    {
+        ptr[i]=&numbers[i];
+    }
+    printf("Vaues using pointer array: \n");
+    for (int i=0;i<5;i++)
+    {
+        printf("%d",*ptr[i]);
+    }
+    return 0;
+}
