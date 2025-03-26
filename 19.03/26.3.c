649863ab943e4a9043df1f293f8e70a565eb0a58
Anshiya Kansal <anshiyakansal.india@gmail.com>
2025-03-26 17:08:16 +0530

    Update 26.3.c

--- 19.03/26.3.c
+++ 19.03/26.3.c
@@ -44,3 +44,36 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+#include <stdio.h>
+int linearSearch(int arr[], int n, int key){
+    for (int i=0; i<n; i++){
+        if (arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n, key;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+    int result = linearSearch(arr, n, key);
+    if (result!=-1){
+        printf("Element found at index %d(position %d)\n", result, result +1);
+    }
+    else {
+        printf("Element not found\n");
+    }
+    return 0;
+}
