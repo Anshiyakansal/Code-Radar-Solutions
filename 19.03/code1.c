4cb4d7f7ecf14f4dbc639b863156ccef84f9a170
Anshiya Kansal <anshiyakansal.india@gmail.com>
2025-03-26 16:49:15 +0530

    Update code1.c

--- 19.03/code1.c
+++ 19.03/code1.c
@@ -19,3 +19,16 @@ int main()
     printf("%d %d %d\n",*ptr[0],*ptr[1],*ptr[2]);
     return 0;
 }
+#include <stdio.h>
+int main()
+{
+    int*p;
+    int val[7]={11,22,33,44,55,66,77};
+    p=&val[0];
+    for(int i=0;i<7;i++)
+    {
+        printf("val[%d]: value is %d and address is %p\n",i,*p,p);
+        p++;
+    }
+    return 0;
+}
